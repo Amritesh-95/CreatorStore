b157364aab8a4efbdd5794d64894d6a26587d60d
amrit <amritesh2002.ak@gmail.com>
2026-05-19 02:07:41 +0530

    chore : add dotenv java for the applciation

---
+++ src/main/java/in/nikam/creatorstore/CreatorStoreApplication.java
@@ -0,0 +1,19 @@
+package in.nikam.creatorstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorStoreApplication {
+
+    //configurations of dotenvjava
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((DotenvEntry entry) -> System.setProperty(
+                entry.getKey(), entry.getValue()
+        ));
+        SpringApplication.run(CreatorStoreApplication.class, args);
+    }
+}
--- src/test/java/in/nikam/creatorstore/CreatorStoreApplicationTests.java
+++ src/test/java/in/nikam/creatorstore/CreatorStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.creatorstore;
+package in.nikam.creatorstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
